b61d51e8ac3debec9f3968ab3146cd51e6860df0
Marcell <alan.marcell@accenture.com>
2017-08-24 02:16:13 -0300

    Moving state to Redux

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { CourseService } from './course.service';
 import { Course } from './course';
 import { FilterTextComponent } from '../blocks/filter-text';
-import { store, filterCourses } from '../store';
+import { filterCourses, IAppState } from '../store';
+import { NgRedux, select } from 'ng2-redux';
+import { Observable } from 'rxjs/Observable';
+import { CourseActions } from './course.actions';
 
 @Component({
   selector: 'app-course-list',
@@ -10,27 +13,21 @@ import { store, filterCourses } from '../store';
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
-  filteredCourses = [];
+  @select('filteredCourses') filteredCourses$: Observable<Course>
 
-  constructor(private _courseService: CourseService) {
+  constructor(
+    private ngRedux: NgRedux<IAppState>,
+    private courseActions: CourseActions
+  ) {
   }
 
   filterChanged(searchText: string) {
     console.log('user searched: ', searchText);
-    store.dispatch(filterCourses(searchText));
+    this.courseActions.filterCourses(searchText);
   }
-
-  updateFromState() {
-    const allState = store.getState();
-    this.filteredCourses = allState.filteredCourses;
-  }
-
 
   ngOnInit() {
-    this.updateFromState();
-    store.subscribe(() => {
-      this.updateFromState()
-    });
+    this.courseActions.getCourses();
     componentHandler.upgradeDom();
   }
 }
--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -1,6 +1,6 @@
 import { Course } from '../courses/course';
 import { IAppState } from './IAppState';
-import { FILTER_COURSES } from './actions'
+import { FILTER_COURSES, REQUEST_COURSES_SUCCESS } from '../courses/course.actions'
 
 const courses = [
   {
@@ -31,10 +31,19 @@ function filterCourses(state, action): IAppState {
   });
 }
 
+function storeCourses(state, action): IAppState {
+  return Object.assign({}, state, {
+    courses: action.courses,
+    filteredCourses: action.courses
+  });
+}
+
 export function reducer(state = initialState, action) {
   switch (action.type) {
     case FILTER_COURSES:
       return filterCourses(state, action)
+    case REQUEST_COURSES_SUCCESS:
+      return storeCourses(state, action)
     default:
       return state;
   }
